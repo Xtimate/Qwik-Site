6b29a06cb66c58860eee239513e07277f74606d7
Xtimate <lordquestionmark.discord@gmail.com>
2024-06-29 11:24:05 +0200

    Fixed

--- src/components/nav/nav.tsx
+++ src/components/nav/nav.tsx
@@ -3,7 +3,7 @@ import { component$ } from "@builder.io/qwik"
 export default component$(() => {
     return (
         <div class="flex sticky width-full bg-inherit text-white top-0 py-3 flex-wrap justify-around bg-silver items-center opacity-75">
-        <h1 class="text-xl font-semibold text-white">KittenHost</h1>`
+        <h1 class="text-xl font-semibold text-white">KittenHost</h1>
         <div class="w-1/2"></div>
         <ul class="flex gap-[40px] text-m text-white">
           <li>Home</li>
