2814585712010f0370404ce7c42dba50c37eb8af
Xtimate <lordquestionmark.discord@gmail.com>
2024-06-29 14:20:07 +0200

    Fixed height

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -8,10 +8,9 @@ export default component$(() => {
       <div class="flex-grow flex items-center justify-center">
         <div class="max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center">
           <div class="max-w-3xl mx-auto text-center">
-            <h1 class="bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text text-2xl font-extrabold text-transparent sm:text-5xl leading-tight">
+            <h1 class="bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text text-2xl font-extrabold text-transparent sm:text-5xl leading-tight h-14">
               Fair hosting for everyone
             </h1>
-
             <p class="mt-4 max-w-xl mx-auto text-base sm:text-xl leading-relaxed">
               At KittenHost, we believe in fair hosting for everyone, unlike big providers. Anyone can afford our hosting without breaking the bank.
             </p>
